58358055428911bf67ef071eb20413916d86eb14
kylincat <1059914928@qq.com>
2018-08-21 15:57:02 +0800

    连接MongoDB后完成项目

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var todoController = require('./controllers/todocontroller');
 
+
 var app = express();
 
 app.set('view engine', 'ejs');
--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -2,22 +2,47 @@ var bodyParser = require('body-parser');
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'swimming'}]
+var mongoose = require('mongoose');
+
+var todoschema = mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoschema)
+
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err) {
+//     if(err) throw err;
+//     console.log("item saved")
+// })
+
+mongoose.connect('mongodb://kylincat:703458onepiece@ds125362.mlab.com:25362/kylincat');
+
+// var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'swimming'}]
 
 module.exports = function(app) {
     app.get('/todo', function(req, res) {
-    res.render('todo', {todos: data});
+        Todo.find({}, function(err, data) {
+            if(err) throw err;
+            res.render('todo', {todos: data});
+        });
     });
 
     app.post('/todo', urlencodedParser, function(req, res) {
-        data.push(req.body);
-        res.json(data);
+        var itemOne = Todo(req.body).save(function(err,data) {
+            if(err) throw err;
+            res.json(data);
+        })
+        
     });
 
     app.delete('/todo/:item', function(req, res) {
-        data = data.filter(function(todo) {
-            return todo.item.replace(/ /g, "-") != req.params.item;
-        });
-        res.json(data);
+        // data = data.filter(function(todo) {
+        //     return todo.item.replace(/ /g, "-") != req.params.item;
+        // });
+        // res.json(data);
+        Todo.find({item: req.params.item.replace(/-/g, " ")}).deleteOne(function(err, data) {
+            if(err) throw err;
+            res.json(data);
+        })
     });
 }
